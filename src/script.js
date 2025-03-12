58042730c1c0f19589624abb02125b2d6950167e
JoaoG09 <joaomellocarvalho120@gmail.com>
2025-03-11 21:29:54 -0300

    Correção da innerText sendo tratada como string

--- src/script.js
+++ src/script.js
@@ -1,11 +1,10 @@
 let count = document.getElementById("count");
- 
- document.getElementById("increment").addEventListener("click" = function() {
-     count.innerText = parseInt(count.innerText) + 1; // BUG 3: Não está atribuindo o novo valor
- });
- 
- document.getElementById("decrement").onclick = function() {
-     count.innerText = count.innerText - 1; // BUG 4: Trata innerText como string
- };
- 
- // BUG 5: Permite valores negativos
+
+document.getElementById("increment").addEventListener("click", function () {
+    count.innerText = parseInt(count.innerText) + 1; 
+});
+
+document.getElementById("decrement").onclick = function () {
+    let newValue = parseInt(count.innerText) - 1;
+
+};
